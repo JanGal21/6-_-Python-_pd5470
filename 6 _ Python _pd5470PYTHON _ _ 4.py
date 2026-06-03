9400942ffcfdfdb3b89811b4c59ad9e42cd6dc69
JanGal21 <jan.galusek@gmail.com>
2026-06-03 14:11:10 +0200

    pierwszy commit

---
+++ 6 _ Python _pd5470PYTHON _ _ 4.py
@@ -0,0 +1,14 @@
+sekwencja_genetyczna = "ATCGTACTA"
+print(type(sekwencja_genetyczna))#<class 'str'>
+Ilość_adeniny =int(3)
+print(Ilość_adeniny)
+print(type(Ilość_adeniny))#<class 'int'>
+Masa_białka = float(3.2)
+print(type(Masa_białka))
+
+
+
+
+
+
+
