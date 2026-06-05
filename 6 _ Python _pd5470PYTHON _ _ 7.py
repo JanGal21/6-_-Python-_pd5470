769bb0b57b02ce1a6bdd3913b3c76b29e58ab0aa
JanGal21 <jan.galusek@gmail.com>
2026-06-05 09:45:03 +0200

    pierwszy commit

---
+++ 6 _ Python _pd5470PYTHON _ _ 7.py
@@ -0,0 +1,21 @@
+sekcja_RNA = ['AUG','UCG','AAG']
+wynik = bool(sekcja_RNA)
+print(wynik)
+sekcja_DNA = ""
+sprawdzenie = bool(sekcja_DNA)
+print(sprawdzenie)
+liczba_nukleotydów1 = 180
+liczba_kodonów = liczba_nukleotydów1/3
+print(liczba_kodonów)
+liczba_nukleotydów = 127
+liczba_kodonów = 127//3
+Nukleotydy_nie_kodujące = 127%3
+print(liczba_kodonów)
+print(Nukleotydy_nie_kodujące)
+liczba_mutacji1 = 10
+liczba_mutacji1 +=5
+print(liczba_mutacji1)
+liczba_mutacji2 = 15
+print(liczba_mutacji1==liczba_mutacji2)
+print(liczba_mutacji1!=liczba_mutacji2)
+
